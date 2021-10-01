88f8befb4bc4c3ec127dc36e4096e3fa8eda3413
Rose Hudson <cv21874@bristol.ac.uk>
2021-10-01 16:41:41 +0000

    Working tokenisation!!!!!!
    Co-authored-by: Jay Parker <uy21797@bristol.ac.uk>

--- parser.hs
+++ parser.hs
@@ -1,12 +1,12 @@
 -- "alice & bob | carol"
 -- "x & ((¬y) | z)"
 
-data NullaryOp = TrueOp | FalseOp deriving (Ord, Eq)
-data UnaryOp = Not deriving (Ord, Eq)
-data BinaryOp = And | Or | Xor | Implies | Equivalent deriving (Ord, Eq)
-data Op = Nullary NullaryOp | Unary UnaryOp | Binary BinaryOp deriving (Ord, Eq)
-data Token = OpToken Op | NameToken String | LeftParen | RightParen
-data SyntaxNode = NullaryNode NullaryOp | UnaryNode UnaryOp SyntaxNode | BinaryNode BinaryOp SyntaxNode SyntaxNode | Name String
+data NullaryOp = TrueOp | FalseOp deriving (Ord, Eq, Show)
+data UnaryOp = Not deriving (Ord, Eq, Show)
+data BinaryOp = And | Or | Xor | Implies | Equivalent deriving (Ord, Eq, Show)
+data Op = Nullary NullaryOp | Unary UnaryOp | Binary BinaryOp deriving (Ord, Eq, Show)
+data Token = OpToken Op | NameToken String | LeftParen | RightParen deriving (Eq, Show)
+data SyntaxNode = NullaryNode NullaryOp | UnaryNode UnaryOp SyntaxNode | BinaryNode BinaryOp SyntaxNode SyntaxNode | Name String deriving (Show)
 
 cumulativeCheck :: (Eq a) => a -> a -> Bool -> Bool
 cumulativeCheck needle element soFar = soFar || needle == element
@@ -33,24 +33,36 @@ eatParen ('(':cs) = (Just LeftParen, cs)
 eatParen (')':cs) = (Just RightParen, cs)
 eatParen x = (Nothing, x)
 
-eatName :: String -> String -> (Maybe String, String)
+eatName :: String -> String -> (Maybe Token, String)
 eatName soFar (c:cs) =
     if contains validNameChars c
         then eatName (soFar ++ [c]) cs
-        else (if soFar == "" then (Nothing, (c:cs)) else (Just soFar, (c:cs)))
+        else (if soFar == "" then (Nothing, (c:cs)) else (Just $ NameToken soFar, (c:cs)))
 eatName [] [] = (Nothing, [])
-eatName soFar [] = (Just soFar, "")
+eatName soFar [] = (Just $ NameToken soFar, "")
 
-eatOp :: String -> (Token, String)
-eatOp ('¬':cs) = (OpToken $ Unary Not, cs)
-eatOp ('&':cs) = (OpToken $ Binary And, cs)
-eatOp ('|':cs) = (OpToken $ Binary Or, cs)
-eatOp ('^':cs) = (OpToken $ Binary Xor, cs)
-eatOp ('⇒':cs) = (OpToken $ Binary Implies, cs)
-eatOp ('≡':cs) = (OpToken $ Binary Equivalent, cs)
+eatOp :: String -> (Maybe Token, String)
+eatOp ('¬':cs) = (Just $ OpToken $ Unary Not, cs)
+eatOp ('&':cs) = (Just $ OpToken $ Binary And, cs)
+eatOp ('|':cs) = (Just $ OpToken $ Binary Or, cs)
+eatOp ('^':cs) = (Just $ OpToken $ Binary Xor, cs)
+eatOp ('⇒':cs) = (Just $ OpToken $ Binary Implies, cs)
+eatOp ('≡':cs) = (Just $ OpToken $ Binary Equivalent, cs)
+eatOp x = (Nothing, x)
+
+eat :: [Token] -> String -> ([Token], String)
+eat tokens x = case eatParen $ eatWhitespace x of
+    (Just t, cs) -> (tokens ++ [t], cs)
+    (Nothing, _) -> case eatName "" $ eatWhitespace x of
+        (Just t, cs) -> (tokens ++ [t], cs)
+        (Nothing, _) -> case eatOp $ eatWhitespace x of
+            (Just t, cs) -> (tokens ++ [t], cs)
+            (Nothing, _) -> undefined
+
+accumulate :: [Token] -> String -> ([Token], String)
+accumulate tokens "" = (tokens, "")
+accumulate tokens s = uncurry accumulate $ eat tokens s
 
 --"x & & y"
---tokenise :: Int -> String -> [Op]
---tokenise 0 "" = Nothing
---tokenise _ "" = undefined
---tokenise 
+tokenise :: String -> [Token]
+tokenise x = fst $ accumulate [] x
