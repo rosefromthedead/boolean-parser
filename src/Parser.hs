cf7bf4746c628eaced653df85904bc34d687e9a8
Rose Hudson <cv21874@bristol.ac.uk>
2021-10-09 14:50:52 +0100

    Simplify `ParseElement` into `ParensTree`
    It's easier not to wrangle the tokens into elements, and instead to put
    them in the tree as is

--- src/Parser.hs
+++ src/Parser.hs
@@ -10,8 +10,8 @@ data Unary = Not deriving (Eq, Show)
 data Binary = And | Or | Xor | Implies | Equivalent deriving (Eq, Show)
 data Op = UnaryOp Unary | BinaryOp Binary deriving (Eq, Show)
 data Token = OpToken Op | NameToken String | LeftParen | RightParen deriving (Eq, Show)
+data ParensTree = ParensNode [ParensTree] | TokenLeaf Token deriving (Show)
 data SyntaxNode = UnaryNode Unary SyntaxNode | BinaryNode SyntaxNode Binary SyntaxNode | NameNode String deriving (Show)
-data ParseElement = ParensElement [ParseElement] | UnaryOpElement Unary | BinaryOpElement Binary | NameElement String deriving (Show)
 
 validNameChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 notChars = "!¬~"
@@ -54,41 +54,34 @@ tokenise :: String -> [Token]
 tokenise "" = []
 tokenise x = let (token, cs) = fromJust $ eatParen x <|> eatName "" x <|> eatOp x in token : tokenise (eatWhitespace cs)
 
-wrangleParens :: Bool -> [Token] -> ([ParseElement], [Token])
+wrangleParens :: Bool -> [Token] -> ([ParensTree], [Token])
 wrangleParens shouldTerminate (LeftParen : ts) =
     let (inner, right) = wrangleParens False ts in
         let (rightElements, rightRemaining) = wrangleParens shouldTerminate right in
-            (ParensElement inner : rightElements, rightRemaining)
+            (ParensNode inner : rightElements, rightRemaining)
 wrangleParens True (RightParen : ts) = undefined -- expected EOF, found ')'
 wrangleParens False (RightParen : ts) = ([], ts)
-wrangleParens shouldTerminate (OpToken (UnaryOp op) : ts) =
-    let (elems, remaining) = wrangleParens shouldTerminate ts in
-        (UnaryOpElement op : elems, remaining)
-wrangleParens shouldTerminate (OpToken (BinaryOp op) : ts) =
-    let (elems, remaining) = wrangleParens shouldTerminate ts in
-        (BinaryOpElement op : elems, remaining)
-wrangleParens shouldTerminate (NameToken name : ts) =
-    let (elems, remaining) = wrangleParens shouldTerminate ts in
-        (NameElement name : elems, remaining)
+wrangleParens shouldTerminate (t : ts) =
+    let (elems, remaining) = wrangleParens shouldTerminate ts in (TokenLeaf t : elems, remaining)
 wrangleParens True [] = ([], [])
 wrangleParens False [] = undefined -- expected ')', found EOF
 
-searchOps :: [ParseElement] -> [ParseElement] -> Binary -> Maybe ([ParseElement], Binary, [ParseElement])
-searchOps left (BinaryOpElement op : es) needle | needle == op = Just (left, op, es)
+searchOps :: [ParensTree] -> [ParensTree] -> Binary -> Maybe ([ParensTree], Binary, [ParensTree])
+searchOps left (TokenLeaf (OpToken (BinaryOp op)) : es) needle | needle == op = Just (left, op, es)
 searchOps left (e:right) needle = searchOps (left ++ [e]) right needle
 searchOps left [] _ = Nothing
 
-wrangleOps :: [ParseElement] -> SyntaxNode
-wrangleOps [ParensElement inner] = wrangleOps inner
-wrangleOps [NameElement name] = NameNode name
-wrangleOps (BinaryOpElement _ : _) = undefined
+wrangleOps :: [ParensTree] -> SyntaxNode
+wrangleOps [ParensNode inner] = wrangleOps inner
+wrangleOps [TokenLeaf (NameToken name)] = NameNode name
+wrangleOps (TokenLeaf (OpToken (BinaryOp _)) : _) = undefined
 wrangleOps list =
     let search = searchOps [] list in
         let result = search Equivalent <|> search Implies <|> search Xor <|> search Or <|> search And in
             case result of
                 Just (left, op, right) -> BinaryNode (wrangleOps left) op (wrangleOps right)
                 Nothing -> case list of
-                    (UnaryOpElement Not : right) -> UnaryNode Not $ wrangleOps right
+                    (TokenLeaf (OpToken (UnaryOp Not)) : right) -> UnaryNode Not $ wrangleOps right
                     _ -> undefined
 
 parse :: String -> SyntaxNode
